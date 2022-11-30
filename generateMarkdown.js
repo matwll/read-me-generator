41ef44efeb67526eeca56bdcac556c6a6d60f33f
Matthew Williams <maw01091989@gmail.com>
2022-11-29 18:27:06 -0800

    added a map to the tech properties so it will list as many as are entered

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,7 +1,6 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  console.log("which license?", license);
   if(license == 'MIT') {
     return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]`
   }else if (license == 'Apache'){
@@ -37,17 +36,17 @@ function renderLicenseLink(license) {
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {
-}
+// function renderLicenseSection(license) {
+// }
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+// function generateMarkdown(data) {
+//   return `# ${data.title}
 
-`;
-}
+// `;
+// }
 
-module.exports = {generateMarkdown, renderLicenseBadge, renderLicenseLink};
+module.exports = {renderLicenseBadge, renderLicenseLink};
 
 //    choices: ['MIT', 'Apache', 'GNU General Public', 'Mozilla Public', 'Eclipse Public'],
 
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
 const fs = require("fs");
-const { generateMarkdown, renderLicenseBadge, renderLicenseLink } = require("./generateMarkdown");
+const { renderLicenseBadge, renderLicenseLink } = require("./generateMarkdown");
 
 // TODO: Create an array of questions for user input
 
